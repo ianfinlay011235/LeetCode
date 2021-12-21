b9cbecb6a4079129825a47644f41feaf9d443eda
Ian Finlay <ian.finlay@hotmail.com>
2021-12-21 16:34:08 -0500

    2021-12-21 solution dump

---
+++ LeetCodeSolutionLibrary.Tests/BitManipulation/PowerOfTwoTests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCodeSolutionLibrary.BitManipulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LeetCodeSolutionLibrary.Tests.BitManipulation
+{
+    public class PowerOfTwoTests
+    {
+        [Theory]
+        [InlineData(1, true)]
+        [InlineData(16, true)]
+        [InlineData(3, false)]
+        public void IsPowerOfTwo_ShouldReturnExpectedValue(int n, bool expected)
+        {
+            //Arrange
+            PowerOfTwo solution = new PowerOfTwo();
+            bool actual;
+
+            //Act
+            actual = solution.IsPowerOfTwo(n);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ LeetCodeSolutionLibrary/BitManipulation/PowerOfTwo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutionLibrary.BitManipulation
+{
+    //https://leetcode.com/problems/power-of-two/
+    public class PowerOfTwo
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            return Math.Log2(n) % 1 == 0;
+        }
+
+        //https://leetcode.com/problems/power-of-two/discuss/63974/Using-nand(n-1)-trick
+        public bool BitManipulation_IsPowerOfTwo(int n)
+        {
+            return ((n & (n - 1)) == 0);
+        }
+    }
+}
