bd293a847399d753dcf248e31eea75449af8348e
Ian Finlay <ian.finlay@cornerstone-bb.com>
2024-01-16 10:16:47 -0500

    Add scaffolding project

---
+++ LeetCodeScaffolder/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LeetCodeScaffolder
+{
+    class Program
+    {
+        const string BASE_DIRECTORY = "C:\\Users\\ian.finlay\\source\\repos\\LeetCode";
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("namespace:");
+            string nameSpace = Console.ReadLine();
+            string path = Path.Combine(BASE_DIRECTORY, "LeetCodeSolutionLibrary", nameSpace);
+            while (!Directory.Exists(path))
+            {
+                Console.WriteLine($"The namespace \"{nameSpace}\" doesn't exist, would you like to create it? (Y/N):");
+                string response = Console.ReadLine();
+                if (response.ToLower() == "y")
+                {
+                    break;
+                }
+                else if (response.ToLower() == "n")
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid response. Enter Y or N.");
+                }
+            }
+
+            Console.WriteLine("url:");
+            string url = Console.ReadLine();
+
+            Console.WriteLine("class name:");
+            string className = Console.ReadLine();
+
+            Console.WriteLine("method signature: example - \"int MethodName(string param1, int param2)\"");
+            string methodSignature = Console.ReadLine();
+
+            var files = new Dictionary<string, FileInfo>();
+
+            files.Add(
+                "SolutionClass", 
+                new FileInfo(Path.Combine(BASE_DIRECTORY, "LeetCodeSolutionLibrary", nameSpace, $"{className}.cs")));
+
+            files.Add(
+                "TestClass",
+                new FileInfo(Path.Combine(BASE_DIRECTORY, "LeetCodeSolutionLibrary.Tests", nameSpace, $"{className}Tests.cs")));
+
+            foreach (var file in files.Values)
+            {
+                file.Directory.Create();
+            }
+
+            Scaffolder s = new Scaffolder();
+            s.Scaffold(files, nameSpace, url, className, methodSignature);
+        }
+    }
+}
---
+++ LeetCodeScaffolder/Scaffolder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace LeetCodeScaffolder
+{
+    public class Scaffolder
+    {
+        public void Scaffold(Dictionary<string, FileInfo> files, string nameSpace, string url, string className, string methodSignature)
+        {
+            ScaffoldSolutionClass(files["SolutionClass"], nameSpace, url, className, methodSignature);
+            ScaffoldTestClass(files["TestClass"], nameSpace, url, $"{className}Tests", methodSignature);
+        }
+
+        public void ScaffoldSolutionClass(FileInfo file, string nameSpace, string url, string className, string methodSignature)
+        {
+            List<string> lines = new List<string>
+            {
+                "using System;",
+                "",
+                $"namespace LeetCodeSolutionLibrary.{nameSpace}",
+                "{",
+                $"\t//{url}",
+                $"\tpublic class {className}",
+                "\t{",
+                $"\t\tpublic {methodSignature}",
+                "\t\t{",
+                "\t\t\tthrow new NotImplementedException();",
+                "\t\t}",
+                "\t}",
+                "}"
+            };
+
+
+            File.WriteAllText(file.FullName, string.Join(Environment.NewLine, lines));
+        }
+
+        public void ScaffoldTestClass(FileInfo file, string nameSpace, string url, string className, string methodSignature)
+        {
+            int msIndexStart = methodSignature.IndexOf(' ') + 1;
+            int msLength = methodSignature.IndexOf('(') - msIndexStart;
+
+            List<string> lines = new List<string>
+            {
+                "using System;",
+                "using Xunit;",
+                "",
+                $"namespace LeetCodeSolutionLibrary.Tests.{nameSpace}",
+                "{",
+                $"\tpublic class {className}",
+                "\t{",
+                $"\t\t[Theory]",
+                $"\t\t[<ClassData or InlineData>]",
+                $"\t\tpublic void {methodSignature.Substring(msIndexStart, msLength)}_Should()",
+                "\t\t{",
+                "\t\t\t//Arrange",
+                "\t\t\t//Act",
+                "\t\t\t//Assert",
+                "\t\t\tthrow new NotImplementedException();",
+                "\t\t}",
+                "\t}",
+                "}"
+            };
+
+            File.WriteAllText(file.FullName, string.Join(Environment.NewLine, lines));
+        }
+    }
+}
